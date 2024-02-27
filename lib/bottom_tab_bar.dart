c0b82ec29ca3515583c40206d54cd2c815abc9c2
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-02-28 03:13:07 +0900

    add: BottomNavigationBar作成

--- lib/bottom_tab_bar.dart
+++ lib/bottom_tab_bar.dart
@@ -27,6 +27,44 @@ class _BottomTabBarState extends State<BottomTabBar> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_sharp),
+            label: 'home',
+            tooltip: 'home',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.signal_cellular_alt),
+            label: 'signal',
+            tooltip: 'signal',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.network_check_sharp),
+            label: 'network',
+            tooltip: 'network',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.battery_charging_full_outlined),
+            label: 'battery',
+            tooltip: 'battery',
+            backgroundColor: Colors.black,
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        enableFeedback: false,
+        iconSize: 25,
+        selectedItemColor: Colors.amber,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        unselectedItemColor: Colors.grey,
+      ),
     );
   }
 }
