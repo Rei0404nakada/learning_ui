290d42fb3ca7afbf07e15bf6872b52f392495074
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-03-01 00:23:49 +0900

    add: IconButtonを実装

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late List<String>? tweet;
+  List<bool> select = [];
+  void selectList() {
+    for (var i = 0; i < tweet!.length; i++) {
+      select.add(false);
+    }
+  }
 
   Future<List<String>?> getText() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -28,6 +34,7 @@ class _HomeState extends State<Home> {
               builder: (BuildContext context,
                   AsyncSnapshot<List<String>?> snapshot) {
                 if (snapshot.hasData) {
+                  selectList();
                   List<String>? data = snapshot.data;
                   int dataLength = data!.length;
                   return Column(
@@ -41,19 +48,77 @@ class _HomeState extends State<Home> {
                               bottom: BorderSide(width: 1, color: Colors.grey),
                             ),
                           ),
-                          child: Text(
-                            data[dataLength - i - 1],
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 45),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      data[dataLength - i - 1],
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.chat_bubble_outline),
+                                    iconSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.cached),
+                                    iconSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        select[dataLength - i - 1] =
+                                            !select[dataLength - i - 1];
+                                        print(i);
+                                      });
+                                    },
+                                    icon: select[dataLength - i - 1]
+                                        ? const Icon(Icons.favorite)
+                                        : const Icon(Icons.favorite_outline),
+                                    iconSize: 18,
+                                    color: select[dataLength - i - 1]
+                                        ? Colors.pink
+                                        : Colors.grey,
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.save_alt_sharp),
+                                    iconSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         )
                       }
                     ],
                   );
                 } else {
-                  return const Text('まだなにもおきていません');
+                  return const Text(
+                    'まだなにもおきていません',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  );
                 }
               },
             ),
